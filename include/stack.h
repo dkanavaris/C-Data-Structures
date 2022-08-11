48c028a215d6a890cf430112f824b7f4e0e780ba
Dimitris Kanavaris <91437902+dkanavaris@users.noreply.github.com>
2022-08-11 19:46:32 +0300

    Moved stack documentation to .h file

--- include/stack.h
+++ include/stack.h
@@ -7,13 +7,40 @@ typedef struct stack {
     list_t *list;
 }stack_t;
 
+/* Initialize a new stack
+ * Returns a pointer to stack_t struct
+ * Params : 
+ * @free_node_data function used to free the list nodes data */
 stack_t *stack_init(void (*free_data)(void *));
 
+/* Destroy a stack
+ * Params : 
+ * @stack to be destroyed */
 void stack_destroy(stack_t *stack);
 
+/* Push data to stack
+ * Params : 
+ * @stack to push data to
+ * @data to be pushed */
+
 int stack_push(stack_t *stack, void *data);
+
+/* Get the stack top element without removing it
+ * Returns the top stack element
+ * Params : 
+ * @stack to get data from*/
 void *stack_peek(stack_t *stack);
+
+/* Get the stack top element while also removing it
+ * Returns the top stack element
+ * Params : 
+ * @stack to get data from*/
 void *stack_pop(stack_t *stack);
+
+/* Check if stack is empty
+ * Returns 1 if stack is empty, 0 otherwise
+ * Params : 
+ * @stack to check*/
 int stack_is_empty(stack_t *stack);
 
 #endif
--- src/stack.c
+++ src/stack.c
@@ -2,11 +2,6 @@
 #include "stack.h"
 #include "list.h"
 
-
-/* Initialize a new stack
- * Returns a pointer to stack_t struct
- * Params : 
- * @free_node_data function used to free the list nodes data */
 stack_t *stack_init(void (*free_data)(void *)){
 
     stack_t *stack = (stack_t *)malloc(sizeof(stack_t));
@@ -22,43 +17,24 @@ stack_t *stack_init(void (*free_data)(void *)){
     return stack;
 }
 
-/* Destroy a stack
- * Params : 
- * @stack to be destroyed */
 void stack_destroy(stack_t *stack){
     list_destroy(stack->list);
     free(stack);
 }
 
-/* Push data to stack
- * Params : 
- * @stack to push data to
- * @data to be pushed */
 int stack_push(stack_t *stack, void *data){
 
 	return list_append(stack->list, data);
 }
 
-/* Get the stack top element without removing it
- * Returns the top stack element
- * Params : 
- * @stack to get data from*/
 void *stack_peek(stack_t *stack){
 	return stack->list->tail->data;
 }
 
-/* Get the stack top element while also removing it
- * Returns the top stack element
- * Params : 
- * @stack to get data from*/
 void *stack_pop(stack_t *stack){
 	return list_pop(stack->list, get_list_size(stack->list) - 1);
 }
 
-/* Check if stack is empty
- * Returns 1 if stack is empty, 0 otherwise
- * Params : 
- * @stack to check*/
 int stack_is_empty(stack_t *stack){
 	return (get_list_size(stack->list) == 0);
 }
